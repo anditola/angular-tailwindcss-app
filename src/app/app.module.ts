fabc95b5d0234ffff6105de47fce8c1c6cd4ed58
Andreas Toellich <andreas.toellich@gmail.com>
2020-09-10 23:26:19 +0000

    Iteration 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router'
+
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'page1', component: Page1Component },
+   { path: 'page2', component: Page2Component },
+   { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+
+}
+
